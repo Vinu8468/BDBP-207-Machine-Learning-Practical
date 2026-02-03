454f56d6552a606db244f87b0d73f66a4e6555a4
vineethdevadiga8468@gmail.com <vineethdevadiga8468@gmail.com>
2026-02-03 16:06:51 +0530

    modified to have both the sigmoid and derivative curve on the same plot

--- Lab5/question3.py
+++ Lab5/question3.py
@@ -2,13 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-X=np.random.randint(-10,10,1000)
+X=np.random.randint(-10,10,100000)
 X=np.sort(X)
-Y=[(1/(1+np.exp(-x))) for x in X]
 
+Y=[(1/(1+np.exp(-x))) for x in X]
 Dir=[np.exp(-x)/((1+np.exp(-x))**2) for x in X]
-plt.plot(X,Dir)
-plt.xlabel("x")
-plt.ylabel("Sigmoid Derivative")
-plt.title("Derivative of Sigmoid Function")
+
+plt.figure() # this will enable to plot both the curves on a single plot.
+plt.plot(X,Y, label="Sigmoid Function")
+plt.plot(X,Dir,label="Derivative of Sigmoid Function")
+plt.xlabel("X values ")
+plt.ylabel("Y values")
+plt.title("Sigmoid Function and its Derivative")
+plt.legend() # this will display the label on the left hand side.
 plt.show()
+
