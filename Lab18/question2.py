4d92df6830a21938fdc562a24e7e62b33018decd
vineethdevadiga8468@gmail.com <vineethdevadiga8468@gmail.com>
2026-04-21 16:47:14 +0530

    classification of iris dataset into 1 vs rest using RBF SVM with just first 2 features

--- Lab18/question2.py
+++ Lab18/question2.py
@@ -4,9 +4,7 @@
 from sklearn.datasets import load_iris
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score
-import pandas as pd
 import numpy as np
-import sys
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
