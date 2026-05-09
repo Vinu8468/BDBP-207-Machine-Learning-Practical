d91ad55fb8b653f6b6c5fb343fb543f4988b727c
vineethdevadiga8468@gmail.com <vineethdevadiga8468@gmail.com>
2026-05-09 15:27:32 +0530

    is the matrix positive definite?

--- Lab27/question1.py
+++ Lab27/question1.py
@@ -1,1 +1,22 @@
 # Can you tell whether the matrix, A = [9  -15;  -15  21] is positive definite?
+import sympy as sp
+A = sp.Matrix([[9,-15],[-15,21]])
+
+print("martix A :")
+print(sp.pprint(A)) # very cool
+
+# eigenvalues
+eigen = A.eigenvals()
+print(f"eigenvalues :{eigen}")
+
+eigs = list(eigen.keys())
+print(f"eigenvectors :{eigs}")
+
+if all(ev >0 for ev in eigs):
+    print("A is Positive Definite")
+elif all(ev >= 0 for ev in eigs):
+    print("\nA is Positive Semi-Definite")
+elif all(ev < 0 for ev in eigs):
+    print("\nA is Negative Definite")
+else:
+    print("\nA is Indefinite")
